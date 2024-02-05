9dc5ff1cb4b3a874338092846bb55aaa3ecad5a0
Robert <90513724+ideen-los@users.noreply.github.com>
2024-02-05 23:27:22 +0100

    ENH: add html template

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Test",
+      template: "./src/template.html",
     }),
   ],
   output: {
